d8771eb743e3752490fcbbba56ecc632dd275994
Raymoon Samir <Raymoon Samir Durango>
2020-12-21 14:19:01 -0500

    Cambios listado de capitulos

--- bd/conexion.php
+++ bd/conexion.php
@@ -2,7 +2,7 @@
 class Conexion{	  
     public static function Conectar() {        
         define('servidor', 'localhost');
-        define('nombre_bd', 'semillero');
+        define('nombre_bd', 'semilleroprueba');
         define('usuario', 'root');
         define('password', '');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
